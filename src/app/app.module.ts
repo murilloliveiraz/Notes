59e7b2e53b3b9ad433b6d09c26482fdabf7fe224
Murillo Oliveira <murillo1047@gmail.com>
2023-08-11 11:08:09 -0300

    created main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
