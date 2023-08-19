2fa222d2672782124a7d7faa65d62c32d100289e
Murillo Oliveira <murillo1047@gmail.com>
2023-08-18 23:30:03 -0300

    feat: routingt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotasComponent } from './components/notas/notas.component';
+import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  {
+    path: 'notas',
+    component: NotasComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
