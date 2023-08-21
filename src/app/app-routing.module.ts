8eb7bdee30d77798aeab00fcd373223fe7d1463c
Murillo Oliveira <murillo1047@gmail.com>
2023-08-21 08:07:41 -0300

    versao antiga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { MainComponent } from './components/main/main.component';
+import { NotasComponent } from './components/notas/notas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MainComponent
+  },
+  {
+    path: 'notas',
+    component: NotasComponent
+  },
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
